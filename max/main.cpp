c975e19c0b030641ddb77c836b6c7f78fafe9d58
卢佳明 <571836353@qq.com>
2015-11-18 18:06:22 +0800

    Initial Commit

---
+++ max/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  max
+//
+//  Created by 20141105055ljm on 15/11/18.
+//  Copyright (c) 2015年 20141105055ljm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
